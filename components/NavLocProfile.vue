be388541b07a5dd59db999e25b7aa6f3653ccf6a
vasily.smolin <vasilysmoolin@gmail.com>
2022-03-26 15:26:15 +0400

    fix

--- components/NavLocProfile.vue
+++ components/NavLocProfile.vue
@@ -1,8 +1,12 @@
 <template>
   <nav class="container mt-[80px] sm:pl-0">
     <ul class="hub-nav flex justify-start sm:justify-center flex-nowrap pb-1 w-full text-sm sm:text-base text-[#6E7191] font-bold">
-      <li class="pl-4"><NuxtLink to="/profile" class="hub-nav-active">Профиль</NuxtLink></li>
-      <li class="pl-4 sm:pl-6"><NuxtLink to="">Обьявления</NuxtLink></li>
+      <li class="pl-4">
+        <NuxtLink to="/profile">Профиль</NuxtLink>
+      </li>
+      <li class="pl-4 sm:pl-6">
+        <NuxtLink to="/add">Обьявления</NuxtLink>
+      </li>
 
       <li v-if="isPerson" class="pl-4 sm:pl-6">
         <NuxtLink to="/vacancies">Вакансии</NuxtLink>
@@ -12,10 +16,16 @@
         <NuxtLink to="/resume">Резюме</NuxtLink>
       </li>
 
-      <li class="pl-4 sm:pl-6"><NuxtLink to="">Услуги</NuxtLink></li>
-      <li class="px-4 sm:pl-6"><NuxtLink to="">Рестораны</NuxtLink></li>
+      <li class="pl-4 sm:pl-6">
+        <NuxtLink to="/uslugi">Услуги</NuxtLink>
+      </li>
+      <li class="px-4 sm:pl-6">
+        <NuxtLink to="/rest">Рестораны</NuxtLink>
+      </li>
 
-      <li v-if="!isPerson"  class="pl-4 sm:pl-6"><NuxtLink to="">Еда</NuxtLink></li>
+      <li v-if="!isPerson"  class="pl-4 sm:pl-6">
+        <NuxtLink to="/eat">Еда</NuxtLink>
+      </li>
 
     </ul>
   </nav>
@@ -46,3 +56,11 @@ export default {
 }
 
 </script>
+
+<style scoped>
+  a.nuxt-link-exact-active {
+    padding-bottom: 3px;
+    color: black;
+    border-bottom: 2px solid black;
+  }
+</style>
--- middleware/redirect.js
+++ middleware/redirect.js
@@ -1,6 +1,6 @@
 export default function ({ $auth, redirect  }) {
         if (!$auth.loggedIn) {
-            this.$auth.fetchUser().then(() => {
+             $auth.fetchUser().then(() => {
                 if (!$auth.loggedIn) {
                     return redirect(process.env.AUTH_URL);
                 }
