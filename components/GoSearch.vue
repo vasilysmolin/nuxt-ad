554313ceb46c81d3edf6f83cc8f3e7372a026ba4
vasily.smolin <vasilysmoolin@gmail.com>
2022-06-04 13:44:16 +0400

    card ad

--- components/GoSearch.vue
+++ components/GoSearch.vue
@@ -15,7 +15,7 @@
       <ul class="pl-6 py-2 w-[100%] list-disc list-inside" v-if="ads.length > 0">
         <!--<li @click="hide" style="list-style-type: none;" v-for="category in categories" class="search-line"><nuxt-link :to="categoryLink(category)" class="text-blue-700 hover:text-black">{{ category.name }}</nuxt-link></li>-->
         <template v-if="ads.length > 0">
-          <li @click="hide" v-for="ad in ads" :key="ad.id" class="search-line"><nuxt-link :to="getUrl(ad)" class="text-blue-700 hover:text-black">{{ ad.name }}</nuxt-link></li>
+          <li @click="hide" v-for="ad in ads" :key="ad.id" class="search-line"><nuxt-link :to="getUrl(ad)" class="text-blue-700 hover:text-black">{{ ad.name }}, цена: {{ ad.price }} руб, адрес: {{ ad.street }} {{ ad.house }}</nuxt-link></li>
         </template>
       </ul>
 
