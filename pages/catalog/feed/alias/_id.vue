518ab35cabaef28083d5bf26b5fb9da72b55aefc
vasily.smolin <vasilysmoolin@gmail.com>
2022-06-09 18:38:34 +0400

    auth

--- pages/catalog/feed/alias/_id.vue
+++ pages/catalog/feed/alias/_id.vue
@@ -6,20 +6,18 @@
         :depth="1"
         :link="adWithCategory"
     />
-<!--    <section class="flex flex-col p-5 w-[95%] rounded-lg sm:max-w-screen-sm">-->
-<!--      <nav class="rounded-md w-full mt-5">-->
-<!--        <ol class="list-reset flex">-->
-<!--          <a class="text-blue-600 hover:text-blue-700" @click="$router.back()">Назад</a>-->
-<!--        </ol>-->
-<!--      </nav>-->
-<!--    </section>-->
+
     <section class="flex flex-col p-5 w-[95%] rounded-lg sm:max-w-screen-sm bg-white">
       <h1 class="first-letter:uppercase font-black text-[0.9375rem] leading-5 sm:text-xl">{{ ad.name }}</h1>
-      <p class="mt-2 text-xl sm:text-2xl font-bold">{{ ad.price }}<span class="pl-2 text-sm">руб.</span></p>
+      <p class="mt-2 text-xl sm:text-2xl font-bold">{{ ad.price }}
+        <span class="pl-2 text-sm">руб.</span>
+      </p>
     </section>
 
     <section class="flex flex-col mt-4 p-5 w-[95%] rounded-lg sm:max-w-screen-sm bg-white">
-      <p class="font-bold text-[0.9375rem] leading-5 sm:text-lg"><span class="pr-2 font-bold text-sm text-gray-500">Контакт:</span>{{ getUsername(ad)}}</p>
+      <p class="font-bold text-[0.9375rem] leading-5 sm:text-lg">
+        <span class="pr-2 font-bold text-sm text-gray-500">Контакт:</span>{{ getUsername(ad)}}
+      </p>
 
       <p v-if="$auth.loggedIn" class="mt-1 font-bold text-[0.9375rem] leading-5 sm:text-lg">
         <span  class="pr-2 font-bold text-sm text-gray-500">
