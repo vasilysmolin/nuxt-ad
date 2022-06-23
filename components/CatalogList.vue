004ebfb41543f10346b159e5777a9b49fe9f51d3
vasily.smolin <vasilysmoolin@gmail.com>
2022-06-23 10:28:16 +0400

    fix

--- components/CatalogList.vue
+++ components/CatalogList.vue
@@ -62,7 +62,7 @@
         expand: 'profile.user',
         alias: alias,
         from: 'catalog',
-        querySearch: querySearch.querySearch
+        querySearch: querySearch
       })" type="button"
               class="w-full inline-block mt-6 px-6 py-2 border-2 border-blue-600 text-blue-600 font-bold text-normal leading-normal rounded hover:border-black hover:text-black focus:outline-none focus:ring-0 transition duration-150 ease-in-out">
         Смотреть дальше
