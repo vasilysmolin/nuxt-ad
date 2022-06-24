1a21cb1faadd54f9442e2304db799b1404b1fc07
vasily.smolin <vasilysmoolin@gmail.com>
2022-06-24 14:00:43 +0400

    fix

--- components/AuthModal.vue
+++ components/AuthModal.vue
@@ -1,21 +1,22 @@
 <template>
-  <modal name="AuthModal"  width="10)%" height="100%" :adaptive="true">
+  <modal name="AuthModal" :min-width="320" :max-width="768" :max-height="400"  width="100%" height="100%" :adaptive="true">
     <section class="flex flex-col justify-center items-center w-full">
       <div class="mt-20 mb-20 w-[75%] text-center">
-        <h1 class="text-lg">Для просмотра телефона, необходимо авторизоваться.</h1>
+        <h1 class="first-letter:uppercase font-black text-[0.9375rem] leading-5 text-xl">Для просмотра телефона,
+          необходимо авторизоваться</h1>
       </div>
       <div>
         <section class="input-group relative flex flex-wrap items-stretch w-full">
           <button type="button" @click.prevent="hide"
                   class="form-control relative flex-auto min-w-0 block w-full
                px-6 py-4 text-base font-bold bg-white bg-clip-padding border
-                border-solid border-gray-300 rounded transition
+                border-solid border-gray-300  rounded transition
                 ease-in-out m-0 focus:font-bold focus:outline-none focus:ring-0"
           >
             Закрыть
           </button>
           <button type="button" @click.prevent="goAuth"
-                  class="form-control text-white bg-green-700 relative flex-auto min-w-0 block w-full
+                  class="form-control text-white bg-green-700 focus:bg-green-600 focus:outline-none relative flex-auto min-w-0 block w-full
                px-6 py-4 text-base font-bold bg-white bg-clip-padding border
                 border-solid border-gray-300 rounded transition
                 ease-in-out m-0 focus:font-bold focus:outline-none focus:ring-0"
