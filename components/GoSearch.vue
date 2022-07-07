86a8d8b25c1e715da29a0468ea1ae727d787589a
kremlev <kremlev@hey.com>
2022-07-07 19:22:45 +0500

    search example

--- components/GoSearch.vue
+++ components/GoSearch.vue
@@ -1,82 +1,167 @@
 <template>
-  <modal name="GoSearch" :min-width="320" :max-width="1024" width="95%" height="auto" :adaptive="true">
-    <section class="flex justify-center items-center w-full">
-      <section class="input-group relative flex flex-wrap items-stretch w-full">
-        <input v-on:input="debounceInput"  v-model="querySearch" type="text"
-               class="form-control relative flex-auto min-w-0 block w-full px-6 py-4 text-base font-bold bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:font-bold focus:outline-none focus:ring-0"
-               placeholder="Поиск" aria-label="Поиск" aria-describedby="button-addon2">
-        <button @click="getSearchQuery"
-            class="btn inline-block px-10 py-1 bg-blue-900 text-white font-bold text-base tracking-wider rounded shadow-md hover:bg-black hover:shadow-lg focus:bg-black  focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out flex items-center"
-            type="button" id="button-addon2">Найти
-        </button>
-      </section>
-    </section>
-    <article class="mt-0 flex justify-start items-start"   v-if="hasItems">
+  <section>
+    <div v-if="$device.isDesktop">
+      <modal name="GoSearch" :min-width="320" :max-width="1024" width="95%" height="auto" :adaptive="true">
+        <section class="flex justify-center items-center w-full">
+          <section class="input-group relative flex flex-wrap items-stretch w-full">
+            <input v-on:input="debounceInput" v-model="querySearch" type="text"
+                   class="form-control relative flex-auto min-w-0 block w-full px-6 py-4 text-base font-bold bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:font-bold focus:outline-none focus:ring-0"
+                   placeholder="Поиск" aria-label="Поиск" aria-describedby="button-addon2">
+            <button @click="getSearchQuery"
+                    class="btn inline-block px-10 py-1 bg-blue-900 text-white font-bold text-base tracking-wider rounded shadow-md hover:bg-black hover:shadow-lg focus:bg-black  focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out flex items-center"
+                    type="button" id="button-addon2">Найти
+            </button>
+          </section>
+        </section>
+        <article class="mt-0 flex justify-start items-start" v-if="hasItems">
 
-      <ul class="pl-6 py-2 w-[100%] list-disc list-inside">
-        <template v-if="ads.length > 0">
-          <li @click="hide" v-for="ad in ads" :key="ad.id" class="search-line">
-            <a :href="getUrl(ad)" class="text-blue-700 hover:text-black">
-              {{ ad.name }}, цена: {{ ad.price }} руб, адрес: {{ ad.street }} {{ ad.house }}
-            </a>
-          </li>
-        </template>
-        <template v-if="services.length > 0">
-          <li @click="hide" v-for="service in services" :key="service.id" class="search-line">
-            <a :href="getUrlServices(service)" class="text-blue-700 hover:text-black">
-              {{ service.name }}, цена: {{ service.price }}
-            </a>
-          </li>
-        </template>
-        <template v-if="resumes.length > 0">
-          <li @click="hide" v-for="resume in resumes" :key="resume.id" class="search-line">
-            <a :href="getUrlResume(resume)" class="text-blue-700 hover:text-black">
-              {{ resume.name }}, цена: {{ resume.price }}
-            </a>
-          </li>
-        </template>
-        <template v-if="vacancies.length > 0">
-          <li @click="hide" v-for="vacancy in vacancies" :key="vacancy.id" class="search-line">
-            <a :href="getUrlVacancy(vacancy)" class="text-blue-700 hover:text-black">
-              {{ vacancy.name }}, цена: {{ vacancy.price }}
-            </a>
-          </li>
-        </template>
+          <ul class="pl-6 py-2 w-[100%] list-disc list-inside">
+            <template v-if="ads.length > 0">
+              <li @click="hide" v-for="ad in ads" :key="ad.id" class="search-line">
+                <a :href="getUrl(ad)" class="text-blue-700 hover:text-black">
+                  {{ ad.name }}, цена: {{ ad.price }} руб, адрес: {{ ad.street }} {{ ad.house }}
+                </a>
+              </li>
+            </template>
+            <template v-if="services.length > 0">
+              <li @click="hide" v-for="service in services" :key="service.id" class="search-line">
+                <a :href="getUrlServices(service)" class="text-blue-700 hover:text-black">
+                  {{ service.name }}, цена: {{ service.price }}
+                </a>
+              </li>
+            </template>
+            <template v-if="resumes.length > 0">
+              <li @click="hide" v-for="resume in resumes" :key="resume.id" class="search-line">
+                <a :href="getUrlResume(resume)" class="text-blue-700 hover:text-black">
+                  {{ resume.name }}, цена: {{ resume.price }}
+                </a>
+              </li>
+            </template>
+            <template v-if="vacancies.length > 0">
+              <li @click="hide" v-for="vacancy in vacancies" :key="vacancy.id" class="search-line">
+                <a :href="getUrlVacancy(vacancy)" class="text-blue-700 hover:text-black">
+                  {{ vacancy.name }}, цена: {{ vacancy.price }}
+                </a>
+              </li>
+            </template>
 
-      </ul>
-      <ul class="pr-6 py-2 w-[50%]">
-        <template v-if="ads.length > 0">
-          <li @click="hide" v-for="ad in ads" :key="ad.id" class="search-line">
-            <a :href="getUrl(ad)" class="search-tags">
-              {{ getNameCat(ad) }}
-            </a>
-          </li>
-        </template>
-        <template v-if="services.length > 0">
-          <li @click="hide" v-for="service in services" :key="service.id" class="search-line">
-            <a :href="getUrlServices(service)" class="search-tags">
-              {{ getNameCat(service) }}
-            </a>
-          </li>
-        </template>
-        <template v-if="resumes.length > 0">
-          <li @click="hide" v-for="resume in resumes" :key="resume.id" class="search-line">
-            <a :href="getUrlResume(resume)" class="search-tags">
-              {{ getNameCat(resume) }}
-            </a>
-          </li>
-        </template>
-        <template v-if="vacancies.length > 0">
-          <li @click="hide" v-for="vacancy in vacancies" :key="vacancy.id" class="search-line">
-            <a :href="getUrlVacancy(vacancy)" class="search-tags">
-              {{ getNameCat(vacancy) }}
-            </a>
-          </li>
-        </template>
-      </ul>
+          </ul>
+          <ul class="pr-6 py-2 w-[50%]">
+            <template v-if="ads.length > 0">
+              <li @click="hide" v-for="ad in ads" :key="ad.id" class="search-line">
+                <a :href="getUrl(ad)" class="search-tags">
+                  {{ getNameCat(ad) }}
+                </a>
+              </li>
+            </template>
+            <template v-if="services.length > 0">
+              <li @click="hide" v-for="service in services" :key="service.id" class="search-line">
+                <a :href="getUrlServices(service)" class="search-tags">
+                  {{ getNameCat(service) }}
+                </a>
+              </li>
+            </template>
+            <template v-if="resumes.length > 0">
+              <li @click="hide" v-for="resume in resumes" :key="resume.id" class="search-line">
+                <a :href="getUrlResume(resume)" class="search-tags">
+                  {{ getNameCat(resume) }}
+                </a>
+              </li>
+            </template>
+            <template v-if="vacancies.length > 0">
+              <li @click="hide" v-for="vacancy in vacancies" :key="vacancy.id" class="search-line">
+                <a :href="getUrlVacancy(vacancy)" class="search-tags">
+                  {{ getNameCat(vacancy) }}
+                </a>
+              </li>
+            </template>
+          </ul>
 
-    </article>
-  </modal>
+        </article>
+      </modal>
+    </div>
+    <div v-if="$device.isMobile">
+      <modal name="GoSearch" width="100%" height="100%">
+        <section class="flex justify-center items-center w-full">
+          <section class="input-group relative flex flex-wrap items-stretch w-full">
+            <input v-on:input="debounceInput" v-model="querySearch" type="text"
+                   class="form-control relative flex-auto min-w-0 block w-full px-6 py-4 text-base font-bold bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:font-bold focus:outline-none focus:ring-0"
+                   placeholder="Поиск" aria-label="Поиск" aria-describedby="button-addon2">
+            <button @click="getSearchQuery"
+                    class="btn inline-block px-10 py-1 bg-blue-900 text-white font-bold text-base tracking-wider rounded shadow-md hover:bg-black hover:shadow-lg focus:bg-black  focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out flex items-center"
+                    type="button" id="button-addon2">Найти
+            </button>
+          </section>
+        </section>
+        <article class="mt-0 flex justify-start items-start" v-if="hasItems">
+
+          <ul class="pl-6 py-2 w-[100%] list-disc list-inside">
+            <template v-if="ads.length > 0">
+              <li @click="hide" v-for="ad in ads" :key="ad.id" class="search-line">
+                <a :href="getUrl(ad)" class="text-blue-700 hover:text-black">
+                  {{ ad.name }}, цена: {{ ad.price }} руб, адрес: {{ ad.street }} {{ ad.house }}
+                </a>
+              </li>
+            </template>
+            <template v-if="services.length > 0">
+              <li @click="hide" v-for="service in services" :key="service.id" class="search-line">
+                <a :href="getUrlServices(service)" class="text-blue-700 hover:text-black">
+                  {{ service.name }}, цена: {{ service.price }}
+                </a>
+              </li>
+            </template>
+            <template v-if="resumes.length > 0">
+              <li @click="hide" v-for="resume in resumes" :key="resume.id" class="search-line">
+                <a :href="getUrlResume(resume)" class="text-blue-700 hover:text-black">
+                  {{ resume.name }}, цена: {{ resume.price }}
+                </a>
+              </li>
+            </template>
+            <template v-if="vacancies.length > 0">
+              <li @click="hide" v-for="vacancy in vacancies" :key="vacancy.id" class="search-line">
+                <a :href="getUrlVacancy(vacancy)" class="text-blue-700 hover:text-black">
+                  {{ vacancy.name }}, цена: {{ vacancy.price }}
+                </a>
+              </li>
+            </template>
+
+          </ul>
+         <!--
+          <ul class="pr-6 py-2 w-[50%]">
+            <template v-if="ads.length > 0">
+              <li @click="hide" v-for="ad in ads" :key="ad.id" class="search-line">
+                <a :href="getUrl(ad)" class="search-tags">
+                  {{ getNameCat(ad) }}
+                </a>
+              </li>
+            </template>
+            <template v-if="services.length > 0">
+              <li @click="hide" v-for="service in services" :key="service.id" class="search-line">
+                <a :href="getUrlServices(service)" class="search-tags">
+                  {{ getNameCat(service) }}
+                </a>
+              </li>
+            </template>
+            <template v-if="resumes.length > 0">
+              <li @click="hide" v-for="resume in resumes" :key="resume.id" class="search-line">
+                <a :href="getUrlResume(resume)" class="search-tags">
+                  {{ getNameCat(resume) }}
+                </a>
+              </li>
+            </template>
+            <template v-if="vacancies.length > 0">
+              <li @click="hide" v-for="vacancy in vacancies" :key="vacancy.id" class="search-line">
+                <a :href="getUrlVacancy(vacancy)" class="search-tags">
+                  {{ getNameCat(vacancy) }}
+                </a>
+              </li>
+            </template>
+          </ul>
+-->
+        </article>
+      </modal>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -85,6 +170,6 @@ import SearchMixin from "~/components/mixins/search.mixin"
 
 export default {
   name: 'GoSearch',
-  mixins: [CategoriesMixin,SearchMixin],
+  mixins: [CategoriesMixin, SearchMixin],
 }
 </script>
