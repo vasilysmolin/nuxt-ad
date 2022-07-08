82b685a6fcecafb237fd53832748e1ce38f13bed
kremlev <kremlev@hey.com>
2022-07-08 15:53:53 +0500

    fix fonts uslugi categories

--- components/ListServicesUslugiCategories.vue
+++ components/ListServicesUslugiCategories.vue
@@ -3,27 +3,27 @@
     <section class="mt-7 grid grid-cols-2 gap-6">
       <article @click="" class="px-2 py-4 flex flex-col justify-start rounded-lg w-[140px] bg-[#FFFFFF]">
           <span class="inline-block w-[10px] h-[10px] rounded-full bg-[#D9D9D9]"></span>
-          <h2 class="mt-6 text-xs font-regular">Ремонт и установка техники</h2>
+          <h2 class="mt-6 text-xs font-medium">Ремонт и установка техники</h2>
       </article>
       <article @click="" class="px-2 py-4 flex flex-col justify-start rounded-lg w-[140px] bg-[#FFFFFF]">
         <span class="inline-block w-[10px] h-[10px] rounded-full bg-[#D9D9D9]"></span>
-        <h2 class="mt-6 text-xs font-regular">Творчество, хобби и рукоделие</h2>
+        <h2 class="mt-6 text-xs font-medium">Творчество, хобби и рукоделие</h2>
       </article>
       <article @click="" class="px-2 py-4 flex flex-col justify-start rounded-lg w-[140px] bg-[#FFFFFF]">
         <span class="inline-block w-[10px] h-[10px] rounded-full bg-[#D9D9D9]"></span>
-        <h2 class="mt-6 text-xs font-regular">Перевозки и курьеры</h2>
+        <h2 class="mt-6 text-xs font-medium">Перевозки и курьеры</h2>
       </article>
       <article @click="" class="px-2 py-4 flex flex-col justify-start rounded-lg w-[140px] bg-[#FFFFFF]">
         <span class="inline-block w-[10px] h-[10px] rounded-full bg-[#D9D9D9]"></span>
-        <h2 class="mt-6 text-xs font-regular">Организация мероприятий</h2>
+        <h2 class="mt-6 text-xs font-medium">Организация мероприятий</h2>
       </article>
       <article @click="" class="px-2 py-4 flex flex-col justify-start rounded-lg w-[140px] bg-[#FFFFFF]">
         <span class="inline-block w-[10px] h-[10px] rounded-full bg-[#D9D9D9]"></span>
-        <h2 class="mt-6 text-xs font-regular">Ремонт и строительство</h2>
+        <h2 class="mt-6 text-xs font-medium">Ремонт и строительство</h2>
       </article>
       <article @click="" class="px-2 py-4 flex flex-col justify-start rounded-lg w-[140px] bg-[#FFFFFF]">
         <span class="inline-block w-[10px] h-[10px] rounded-full bg-[#D9D9D9]"></span>
-        <h2 class="mt-6 text-xs font-regular">Артисты</h2>
+        <h2 class="mt-6 text-xs font-medium">Артисты</h2>
       </article>
     </section>
 
