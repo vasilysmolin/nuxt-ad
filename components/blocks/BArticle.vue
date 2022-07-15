fc44b6b0f73911d385b38d4da55d2683e06c2333
kremlev <kremlev@hey.com>
2022-07-15 10:58:12 +0500

    fix card desktop

--- components/blocks/BArticle.vue
+++ components/blocks/BArticle.vue
@@ -3,7 +3,7 @@
     <div v-if="$device.isDesktop">
       <nuxt-link :to="categoryLink(category)">
         <article v-if="isLoading"
-                 class="px-2 py-4 flex flex-col justify-start rounded-lg w-[160px] min-h-[100px] bg-[#FFFFFF] border border-solid border-white transition duration-150 ease-in-out hover:border-[#cccccc]">
+                 class="px-2 py-4 flex flex-col justify-start rounded-lg w-[160px] min-h-[110px] bg-[#FFFFFF] border border-solid border-white transition duration-150 ease-in-out hover:border-[#cccccc]">
           <span class="inline-block w-[10px] h-[10px] rounded-full bg-[#D9D9D9]"></span>
           <h2 class="mt-6 text-sm font-medium">{{ category.name }}</h2>
         </article>
