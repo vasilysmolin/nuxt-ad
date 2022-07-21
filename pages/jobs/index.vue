10fd1081444846000c29e9f1b239b0b5c6f9010c
kremlev <kremlev@hey.com>
2022-07-21 12:06:16 +0500

    jobs index block another service

--- pages/jobs/index.vue
+++ pages/jobs/index.vue
@@ -11,6 +11,29 @@
           Создать аккаунт
         </nuxt-link>
       </p>
+
+      <!-- вставка для перелинка сервисов -->
+      <div class="mt-12">
+        <h3 class="mb-7 text-center font-medium">Другие сервисы</h3>
+        <div class="flex flex-row justify-between items-center w-full">
+          <article @click="uslugi" class="p-3 flex flex-col rounded-lg w-[366px] bg-[#FFECD1] cursor-pointer transition duration-150 ease-in-out hover:bg-[#FFD9A1]">
+            <IconListServicesIndexUslugi/>
+            <div>
+              <h2 class="mt-4 text-base font-black">Услуги</h2>
+              <h3 class="mt-1 text-[12px] leading-normal">Найти исполнителя или создать услугу.</h3>
+            </div>
+          </article>
+          <article @click="catalog" class="p-3 flex flex-col rounded-lg w-[366px] bg-[#FFF4D2] cursor-pointer transition duration-150 ease-in-out hover:bg-[#FFE69B]">
+            <IconListServicesIndexCatalog/>
+            <div>
+              <h2 class="mt-4 text-base font-black">Объявления</h2>
+              <h3 class="mt-1 text-[12px] leading-normal">Продать, купить и многое другое.</h3>
+            </div>
+          </article>
+        </div>
+      </div>
+      <!-- -->
+
       <WhatsTapigo/>
       <BNewLetters/>
     </div>
@@ -26,6 +49,31 @@
         <nuxt-link to="" class="btn btn-primary inline-block mt-7 px-5 py-3 bg-blue-900 text-white font-bold text-sm tracking-wider leading-snug rounded focus:outline-none focus:ring-0">Создать аккаунт
         </nuxt-link>
       </p>
+
+      <!-- вставка для перелинка сервисов -->
+      <div class="mt-7">
+        <h3 class="mb-5 text-center font-medium text-sm font-bold">Другие сервисы</h3>
+        <section class="grid grid-cols-2 gap-6">
+          <article @click="uslugi"
+                   class="px-2 py-5 flex flex-col justify-between rounded-lg w-[140px] h-[140px] bg-[#FFEBCD]">
+            <IconListServicesIndexUslugi/>
+            <div>
+              <h2 class="text-sm font-black">Услуги</h2>
+              <h3 class="mt-1 text-[11px] leading-snug">Найти исполнителя или создать услугу.</h3>
+            </div>
+          </article>
+          <article @click="catalog"
+                   class="px-2 py-5 flex flex-col justify-between rounded-lg w-[140px] h-[140px] bg-[#FFF4D2]">
+            <IconListServicesIndexCatalog/>
+            <div>
+              <h2 class="text-sm font-black">Объявления</h2>
+              <h3 class="mt-1 text-[11px] leading-snug">Продать, купить и многое другое.</h3>
+            </div>
+          </article>
+        </section>
+      </div>
+      <!-- -->
+
       <WhatsTapigo/>
       <BNewLetters/>
     </div>
@@ -39,8 +87,10 @@ import SearchColorGlobalMobile from "../../components/SearchColorGlobalMobile";
 import ListServicesJobs from "../../components/ListServicesJobs";
 import GoSearchDesktop from "../../components/GoSearchDesktop";
 import BNewLetters from "../../components/blocks/BNewLetters";
+import IconListServicesIndexUslugi from "../../components/icons/IconListServicesIndexUslugi";
+import IconListServicesIndexCatalog from "../../components/icons/IconListServicesIndexCatalog";
 export default {
-  components: {GoSearchDesktop, SearchColorGlobalMobile, ListServicesJobs, BNewLetters, WhatsTapigo},
+  components: {GoSearchDesktop, SearchColorGlobalMobile, ListServicesJobs, BNewLetters, WhatsTapigo, IconListServicesIndexUslugi, IconListServicesIndexCatalog},
   head: {
     title: "Бесплатно создавайте вакансии и резюме без ограничений на Tapigo.ru | Работа",
     meta: [
@@ -58,6 +108,12 @@ export default {
     },
     showGoSearch () {
       this.$modal.show('GoSearch');
+    },
+    uslugi() {
+      document.location.href = process.env.USLUGI_URL;
+    },
+    catalog() {
+      document.location.href = process.env.CATALOG_URL;
     },
   }
 }
