252e6fde2e8dc194674114da7ecd85c22fb03291
vasily.smolin <vasilysmoolin@gmail.com>
2022-08-09 13:15:46 +0400

    aviasales and hotels

--- pages/travel/index.vue
+++ pages/travel/index.vue
@@ -1,8 +1,8 @@
 <template>
   <section>
-    <div  class="container flex flex-col mt-[70px] pb-[100px] max-w-3xl min-w-[768px]">
+    <div  class="container mt-[100px] pb-[100px]">
 <!--      <h1>Путешествия</h1>-->
-      <iframe scrolling="no" width="" height="245" frameborder="0" src="//www.travelpayouts.com/widgets/4b6413dc51c5e62355aa21761e972d1d.html?v=2199"></iframe>
+      <script charset="utf-8" src="//www.travelpayouts.com/widgets/4b6413dc51c5e62355aa21761e972d1d.js?v=2199" async></script>
     </div>
   </section>
 </template>
