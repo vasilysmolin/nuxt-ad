63b95f2a94a391d20b0180b0dcebc44dc536fb49
vasily.smolin <vasilysmoolin@gmail.com>
2022-08-26 13:41:38 +0400

    forms

--- pages/travel/index.vue
+++ pages/travel/index.vue
@@ -1,8 +1,12 @@
 <template>
   <section>
-    <div  class="container mt-[100px] pb-[100px]">
+    <div class="container mt-[100px] pb-[100px]">
       <!--      <h1>Путешествия</h1>-->
-      <script charset="utf-8" src="//www.travelpayouts.com/widgets/4b6413dc51c5e62355aa21761e972d1d.js?v=2199" async></script>
+      <script charset="utf-8" src="//www.travelpayouts.com/widgets/4b6413dc51c5e62355aa21761e972d1d.js?v=2201"
+              async></script>
+      <div class="mt-20"></div>
+      <script charset="utf-8" src="//www.travelpayouts.com/widgets/1d5a726e34ef62c161ec5c792bef291c.js?v=2265"
+              async></script>
     </div>
   </section>
 </template>
