8f59dcdb2b6f59eab0a21c0ffd4a9606056d75c2
kremlev <kremlev@hey.com>
2022-08-26 17:55:13 +0500

    fix travel_url + hostels script

--- pages/hostels/index.vue
+++ pages/hostels/index.vue
@@ -2,7 +2,7 @@
   <section>
     <div class="container mt-[100px] pb-[100px]">
       <h1>Hostels</h1>
-      <script charset="utf-8" src="//www.travelpayouts.com/widgets/4b6413dc51c5e62355aa21761e972d1d.js?v=2199"
+      <script charset="utf-8" src="//www.travelpayouts.com/widgets/1d5a726e34ef62c161ec5c792bef291c.js?v=2201"
               async></script>
     </div>
   </section>
