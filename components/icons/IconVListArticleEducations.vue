e149f9c64b2b7d9517f8568fdf2fb9d162b9cf56
kremlev <kremlev@hey.com>
2022-09-20 16:51:21 +0500

    VList Article Preview

---
+++ components/icons/IconVListArticleEducations.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M20 2C22 2 22 4 22 4V7H18M20 2C18 2 18 4 18 4V7M20 2H7C5 2 5 4 5 4V17M18 7V20C18 20 18 22 16 22M16 22C14 22 14 20 14 20V17H2V20C2 20 2 22 4 22H16ZM9 8H14M9 12H14" stroke="#14142B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "IconVListArticleEducations",
+}
+</script>
---
+++ components/icons/IconVListArticleExperiences.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M3 14V22M9 12V22M15 8V22M21 2V22" stroke="#14142B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "IconVListArticleExperiences",
+}
+</script>
